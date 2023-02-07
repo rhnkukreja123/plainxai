4899a4b8cad13a305d4642cd95439e500db28d88
rhnkukreja123 <110974614+rhnkukreja123@users.noreply.github.com>
2023-02-07 15:34:30 +0530

    Add files via upload

---
+++ st_trial.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+import streamlit as st
+
+st.title('Hello World. This is a Title')
+st.subheader("Here, a subheader")
+st.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum")
+st.write("This is can be used for text and other features")
+
+
+
